6e845767bee85386c7aaba2a41804b71877e79d4
lzyszds <1024327189@qq.com>
2024-10-29 15:08:46 +0800

    🎉feat：添加size字段

--- src/components/LzyIcon.vue
+++ src/components/LzyIcon.vue
@@ -5,13 +5,17 @@ interface Props {
   name: string;
   height?: string;
   style?: string | Object;
+  size?: number;
 }
-
 const props = defineProps<Props>();
+let style = props.style;
+if (props.size) {
+  style = Object.assign(props.style || {}, { fontSize: props.size });
+}
 </script>
 
 <template>
-  <Icon class="LzyIcon" :icon="name" :style="props.style"></Icon>
+  <Icon class="LzyIcon" :icon="name" :style="style"></Icon>
 </template>
 
 <style scoped>
