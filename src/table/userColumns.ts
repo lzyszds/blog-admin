f0b35733294b0c8a7f700839fe7f7cd6dfbf5e99
lzyszds <1024327189@qq.com>
2024-11-13 21:36:18 +0800

    ✨fix：给用户表登陆日期转换成中文

--- src/table/userColumns.ts
+++ src/table/userColumns.ts
@@ -1,6 +1,5 @@
 import { Avatar, Button, message, Popconfirm, Tag } from "ant-design-vue";
 import { Column, Params } from "@/typings/Column";
-
 export const getUserColumns = (params: Params) => {
   let columns: Column[] = [
     {
@@ -82,8 +81,24 @@ export const getUserColumns = (params: Params) => {
       width: "100px",
       customRender: ({ text }) => {
         if (!text) return "暂无";
-        return useTimeAgo(text).value;
-        // return useDateFormat(text, 'YYYY-MM-DD').value
+
+        return useTimeAgo(text, {
+          messages: {
+            justNow: '刚刚',
+            past: n => (n.match(/\d/) ? `${n}前` : n),
+            future: n => (n.match(/\d/) ? `在${n}` : n),
+            month: (n, past) => (n === 1 ? (past ? '上个月' : '下个月') : `${n}月`),
+            year: (n, past) => (n === 1 ? (past ? '去年' : '明年') : `${n}年`),
+            day: (n, past) => (n === 1 ? (past ? '昨天' : '明天') : `${n}天`),
+            week: (n, past) => (n === 1 ? (past ? '上周' : '下周') : `${n}周`),
+            hour: n => `${n}小时`,
+            minute: n => `${n}分钟`,
+            second: n => `${n}秒`,
+          },
+          // 其他选项:
+          showSecond: true,
+        }).value;
+
       },
     },
     {
