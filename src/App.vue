fe2214c53a5b817fb1c7f8214bebdce49976cd7b
lzyszds <1024327189@qq.com>
2024-11-13 23:18:27 +0800

    🎉feat：导入鸿蒙字体、配置全局antdv语言为中文

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { message } from 'ant-design-vue';
+import { message } from "ant-design-vue";
+import zhCN from "ant-design-vue/es/locale/zh_CN";
+
 
 const themeMode = useStorage<"light" | "dark">("themeMode", "light");
 
@@ -19,6 +21,8 @@ const theme = computed(() => {
       colorTextHeading: isDark ? "#FFFFFF" : "#000000",
       colorTextPlaceholder: isDark ? "#FFFFFF75" : "#00000080",
       colorFillSecondary: isDark ? "rgb(255 255 255 / 0.12)" : "#00000010",
+      fontFamily:
+        "PingFang SC,HarmonyOS_Regular,Helvetica Neue,Microsoft YaHei,sans-serif",
     },
   };
 });
@@ -27,13 +31,13 @@ message.config({
   duration: 2,
   maxCount: 3,
   rtl: true,
-  prefixCls: 'my-message',
+  prefixCls: "my-message",
 });
 </script>
 
 <template>
   <!-- antd 配置基础样式 -->
-  <AConfigProvider :theme="theme">
+  <AConfigProvider :theme="theme" :locale="zhCN">
     <RouterView />
   </AConfigProvider>
 </template>
