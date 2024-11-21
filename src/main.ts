a8a9655396de71dd01052758d2a2fc6d67e5b82d
lzyszds <1024327189@qq.com>
2024-11-22 01:15:56 +0800

    🎉feat： 解决github actions打包的时候识别不到swiper/css

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
-import "swiper/css";
-import App from './App.vue'
 
-import { setupRouter } from './router'
-import { setupStore } from './store'
-import { setupLoading, unLoad } from '@/plugins'
+import App from "./App.vue";
+
+import { setupRouter } from "./router";
+import { setupStore } from "./store";
+import { setupLoading, unLoad } from "@/plugins";
 
 async function setupApp() {
-
   const app = createApp(App);
   setupLoading();
-  setupStore(app)
-  await setupRouter(app)
+  setupStore(app);
+  await setupRouter(app);
 
   // vue3全局变量挂载
-  app.mount('#app')
+  app.mount("#app");
 
-
-  unLoad()
+  unLoad();
 }
 
 setupApp();
