0dfe113e7ae2ba3057d167c87fd2bbae8a29e3cf
lzyszds <1024327189@qq.com>
2024-12-01 23:00:04 +0800

    ✨feat：更换编辑器（ckeditor）、集成markdown-it、shiki

---
+++ src/components/ckeditor/ckeditor.ts
@@ -0,0 +1,178 @@
+import {
+  Autoformat,
+  Base64UploadAdapter,
+  BlockQuote,
+  Bold,
+  CloudServices,
+  Code,
+  CodeBlock,
+  Essentials,
+  Font,
+  Heading,
+  HorizontalLine,
+  Image,
+  ImageToolbar,
+  ImageUpload,
+  Italic,
+  Link,
+  List,
+  Markdown,
+  Mention,
+  Paragraph,
+  ShowBlocks,
+  SourceEditing,
+  Strikethrough,
+  Table,
+  TableToolbar,
+  TextTransformation,
+  TodoList,
+  Underline,
+} from "ckeditor5";
+import coreTranslations from "ckeditor5/translations/zh-cn.js";
+import premiumFeaturesTranslations from "ckeditor5-premium-features/translations/zh-cn.js";
+
+import "ckeditor5/ckeditor5.css";
+import "ckeditor5-premium-features/ckeditor5-premium-features.css";
+
+export const editorConfig: any = {
+  translations: [coreTranslations, premiumFeaturesTranslations],
+  plugins: [
+    Autoformat, // 自动格式化
+    BlockQuote, // 引用
+    Bold, // 加粗
+    CloudServices, // 云服务
+    Code, // 代码
+    Font, // 字体
+    CodeBlock, // 代码块
+    Essentials, // 基础
+    Heading, // 标题
+    HorizontalLine, // 水平线
+    Image, // 图片
+    ImageToolbar, // 图片工具栏
+    ImageUpload, // 图片上传
+    Base64UploadAdapter, // 图片上传
+    Italic, // 斜体
+    Link, // 链接
+    List, // 列表
+    Markdown, // Markdown
+    Mention, // 提及
+    Paragraph, // 段落
+    Table, // 表格
+    TableToolbar, // 表格工具栏
+    TextTransformation, // 文本转换
+    TodoList, // 待办事项
+    SourceEditing, // 源码编辑
+    ShowBlocks, // 显示html代码
+    Strikethrough, // 删除线
+    Underline, // 下划线
+  ],
+  language: "zh-cn",
+  toolbar: [
+    "undo",
+    "redo",
+    "|",
+    "sourceEditing",
+    "showBlocks",
+    "|",
+    "heading",
+    "|",
+    {
+      label: "Basic styles",
+      icon: "text",
+      items: [
+        "fontSize",
+        "fontFamily",
+        "fontColor",
+        "fontBackgroundColor",
+        "code",
+        "|",
+        "textPartLanguage",
+      ],
+    },
+    "bold",
+    "italic",
+    "underline",
+    "strikethrough",
+    "|",
+    "bulletedList",
+    "numberedList",
+    "todoList",
+    "|",
+    "link",
+    "uploadImage",
+    "insertTable",
+    "blockQuote",
+    "codeBlock",
+    "horizontalLine",
+    "specialCharacters",
+  ],
+  codeBlock: {
+    languages: [
+      { language: "html", label: "html" },
+      { language: "css", label: "css" },
+      { language: "js", label: "js" },
+      { language: "jsx", label: "jsx" },
+      { language: "ts", label: "ts" },
+      { language: "tsx", label: "tsx" },
+      { language: "vue", label: "vue" },
+      { language: "json", label: "json" },
+      { language: "sh", label: "sh" },
+      { language: "nginx", label: "nginx" },
+      { language: "xml", label: "xml" },
+      { language: "markdown", label: "markdown" },
+      { language: "yaml", label: "yaml" },
+    ],
+  },
+  heading: {
+    options: [
+      {
+        model: "paragraph",
+        title: "原文",
+        class: "ck-heading_paragraph",
+      },
+      {
+        model: "heading1",
+        view: "h1",
+        title: "一级标题",
+        class: "ck-heading_heading1",
+      },
+      {
+        model: "heading2",
+        view: "h2",
+        title: "二级标题",
+        class: "ck-heading_heading2",
+      },
+      {
+        model: "heading3",
+        view: "h3",
+        title: "三级标题",
+        class: "ck-heading_heading3",
+      },
+      {
+        model: "heading4",
+        view: "h4",
+        title: "四级标题",
+        class: "ck-heading_heading4",
+      },
+      {
+        model: "heading5",
+        view: "h5",
+        title: "五级标题",
+        class: "ck-heading_heading5",
+      },
+      {
+        model: "heading6",
+        view: "h6",
+        title: "六级标题",
+        class: "ck-heading_heading6",
+      },
+    ],
+  },
+  image: {
+    toolbar: ["imageTextAlternative"],
+  },
+  table: {
+    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+  },
+
+};
--- src/components/markdown/AsyncMarkdownPreview.vue
+++ src/components/markdown/AsyncMarkdownPreview.vue
@@ -5,38 +5,8 @@
 </template>
 
 <script setup>
-import MarkdownIt from "markdown-it";
-import {createHighlighterCore} from "shiki/core";
-import {createOnigurumaEngine} from "shiki/engine/oniguruma";
-import {fromHighlighter} from "@shikijs/markdown-it/core";
-import {Fancybox} from "@fancyapps/ui"; //图片放大
-
-// 手动导入每个语言模块
-import htmlLang from "shiki/langs/html.mjs";
-import cssLang from "shiki/langs/css.mjs";
-import jsLang from "shiki/langs/javascript.mjs";
-import jsxLang from "shiki/langs/jsx.mjs";
-import tsLang from "shiki/langs/typescript.mjs";
-import tsxLang from "shiki/langs/tsx.mjs";
-import vueLang from "shiki/langs/vue.mjs";
-import jsonLang from "shiki/langs/json.mjs";
-import shLang from "shiki/langs/shellscript.mjs";
-import nginxLang from "shiki/langs/nginx.mjs";
-import xmlLang from "shiki/langs/xml.mjs";
-import markDownLang from "shiki/langs/markdown.mjs";
-import yamlLang from "shiki/langs/yaml.mjs";
-
-
-/* 插件 */
-import mdMark from "markdown-it-mark"; // 高亮标记
-import mdAttrs from "markdown-it-attrs"; // 添加自定义属性
-import {full as mdEmoji} from 'markdown-it-emoji' // emoji
-import mdBracketedSpans from "markdown-it-bracketed-spans"; // 括号跨度
-import mdInlineComments from "markdown-it-inline-comments"; // 行内注释
-import mdTipsCollectPlugin from "./plugin/tipsCollect";
-import mdImagePlugin from "./plugin/image";
-import setDirectoryId from "./plugin/directoryid";
-
+import { Fancybox } from "@fancyapps/ui";
+import md from "@/utils/markdownInit.js"; //图片放大
 
 const props = defineProps({
   markdownInput: String,
@@ -44,61 +14,7 @@ const props = defineProps({
 
 const emit = defineEmits(["renderedHtml"]);
 const preview = templateRef("preview"); // 用于渲染markdown的容器
-const md = MarkdownIt({
-  html: true,
-  linkify: true,
-  typographer: true,
-  breaks: true,
-});
 
-
-// 批量导入所有语言模块
-const langModules = [
-  htmlLang,
-  cssLang,
-  jsLang,
-  jsxLang,
-  tsLang,
-  tsxLang,
-  vueLang,
-  jsonLang,
-  shLang,
-  nginxLang,
-  xmlLang,
-  markDownLang,
-  yamlLang,
-];
-
-const highlighter = await createHighlighterCore({
-  themes: [import("shiki/themes/one-dark-pro.mjs")],
-  langs: langModules,
-  engine: createOnigurumaEngine(import("shiki/wasm")),
-});
-
-/* 自定义容器插件 折叠面板 */
-
-/* shiki代码块高亮插件 */
-md.use(
-    fromHighlighter(highlighter, {
-      theme: "one-dark-pro",
-    })
-);
-/* 自定义属性插件 attrs */
-md.use(mdAttrs, {
-  // optional, these are default options
-  leftDelimiter: "{",
-  rightDelimiter: "}",
-  allowedAttributes: [], // empty array = all attributes are allowed
-});
-md.use(mdMark);
-md.use(mdEmoji);
-md.use(mdBracketedSpans);
-md.use(mdInlineComments);
-md.use(mdImagePlugin, {
-  baseUrl: import.meta.env.VITE_BASE_URL,
-});
-setDirectoryId(md);
-mdTipsCollectPlugin(md);
 /* 图片放大插件绑定 */
 const fancyboxBind = () => {
   Fancybox.bind(preview.value, {
@@ -112,18 +28,18 @@ const fancyboxBind = () => {
 
 const renderedHtml = ref("");
 watch(
-    () => props.markdownInput,
-    (newMarkdown) => {
-      try {
-        const data = md.render(newMarkdown || "");
-        renderedHtml.value = data;
-        emit("renderedHtml", data);
-        fancyboxBind();
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    {immediate: true}
+  () => props.markdownInput,
+  (newMarkdown) => {
+    try {
+      const data = md.render(newMarkdown || "");
+      renderedHtml.value = data;
+      emit("renderedHtml", data);
+      fancyboxBind();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  { immediate: true },
 );
 
 onMounted(() => {
