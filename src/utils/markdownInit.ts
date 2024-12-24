2113b7e1e1ce0b300b63d101771ecdd72c1adda1
lzyszds <1024327189@qq.com>
2024-12-24 16:58:01 +0800

    🐛 fix(shiki): 修复异步加载导致无法生成 HTML 的问题, 修改配置文件

--- src/utils/markdownInit.ts
+++ src/utils/markdownInit.ts
@@ -1,10 +1,10 @@
 import MarkdownIt from "markdown-it";
-import { createHighlighterCore } from "shiki/core";
-import { createOnigurumaEngine } from "shiki/engine/oniguruma";
-import { fromHighlighter } from "@shikijs/markdown-it/core";
+import {createHighlighterCore} from "shiki/core";
+import {createOnigurumaEngine} from "shiki/engine/oniguruma";
+import {fromHighlighter} from "@shikijs/markdown-it/core";
 import mdAttrs from "markdown-it-attrs";
 import mdMark from "markdown-it-mark";
-import { full as mdEmoji } from "markdown-it-emoji";
+import {full as mdEmoji} from "markdown-it-emoji";
 import mdBracketedSpans from "markdown-it-bracketed-spans";
 import mdInlineComments from "markdown-it-inline-comments";
 import mdImagePlugin from "@/components/markdown/plugin/image.ts";
@@ -25,61 +25,60 @@ import nginxLang from "shiki/langs/nginx.mjs";
 import xmlLang from "shiki/langs/xml.mjs";
 import markDownLang from "shiki/langs/markdown.mjs";
 import yamlLang from "shiki/langs/yaml.mjs";
-import { HighlighterCore } from "shiki";
+import {HighlighterCore} from "shiki";
 
 const md = MarkdownIt({
-  html: true,
-  linkify: true,
-  typographer: true,
-  breaks: true,
+    html: true,
+    linkify: true,
+    typographer: true,
+    breaks: true,
 });
 
 // 批量导入所有语言模块
 const langModules = [
-  htmlLang,
-  cssLang,
-  jsLang,
-  jsxLang,
-  tsLang,
-  tsxLang,
-  vueLang,
-  jsonLang,
-  shLang,
-  nginxLang,
-  xmlLang,
-  markDownLang,
-  yamlLang,
+    htmlLang,
+    cssLang,
+    jsLang,
+    jsxLang,
+    tsLang,
+    tsxLang,
+    vueLang,
+    jsonLang,
+    shLang,
+    nginxLang,
+    xmlLang,
+    markDownLang,
+    yamlLang,
 ];
 
-createHighlighterCore({
-  themes: [import("shiki/themes/one-dark-pro.mjs")],
-  langs: langModules,
-  engine: createOnigurumaEngine(import("shiki/wasm")),
-}).then((highlighter: HighlighterCore) => {
-  /* 自定义容器插件 折叠面板 */
+const highlighter: HighlighterCore = await createHighlighterCore({
+    themes: [import("shiki/themes/one-dark-pro.mjs")],
+    langs: langModules,
+    engine: createOnigurumaEngine(import("shiki/wasm")),
+})
+/* 自定义容器插件 折叠面板 */
 
-  /* shiki代码块高亮插件 */
-  md.use(
+/* shiki代码块高亮插件 */
+md.use(
     fromHighlighter(highlighter, {
-      theme: "one-dark-pro",
+        theme: "one-dark-pro",
     }),
-  );
-  /* 自定义属性插件 attrs */
-  md.use(mdAttrs, {
+);
+/* 自定义属性插件 attrs */
+md.use(mdAttrs, {
     // optional, these are default options
     leftDelimiter: "{",
     rightDelimiter: "}",
     allowedAttributes: [], // empty array = all attributes are allowed
-  });
-  md.use(mdMark);
-  md.use(mdEmoji);
-  md.use(mdBracketedSpans);
-  md.use(mdInlineComments);
-  md.use(mdImagePlugin, {
-    baseUrl: import.meta.env.VITE_BASE_URL,
-  });
-  setDirectoryId(md);
-  mdTipsCollectPlugin(md);
 });
+md.use(mdMark);
+md.use(mdEmoji);
+md.use(mdBracketedSpans);
+md.use(mdInlineComments);
+md.use(mdImagePlugin, {
+    baseUrl: import.meta.env.VITE_BASE_URL,
+});
+setDirectoryId(md);
+mdTipsCollectPlugin(md);
 
 export default md;
