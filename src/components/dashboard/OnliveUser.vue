1edf3edd5b2f0aeb63f6862d975ceb4da8982537
lzyszds <1024327189@qq.com>
2024-12-24 11:46:01 +0800

    ♻️refactor(components): 优化在线用户组件布局和展示效果
    - 使用 grid 布局优化用户列表，缩小每个用户的展示区域
    - 添加 tooltip 组件，鼠标悬停时显示用户名和最后登录 IP- 调整卡片样式，增加 padding 以适应新布局- 移除多余标签，简化模板结构

--- src/components/dashboard/OnliveUser.vue
+++ src/components/dashboard/OnliveUser.vue
@@ -1,28 +1,33 @@
 <script lang="ts" setup>
-import { useUserOnlineStore } from "@/store/useUserOnline";
+import {useUserOnlineStore} from "@/store/useUserOnline";
 
 const userOnlineStore = useUserOnlineStore();
 </script>
 <template>
   <div class="onliveUser">
-    <ACard v-for="item in userOnlineStore.userOnline">
-      <ABadge status="processing" color="green" :dot="true">
-        <AAvatar :src="item.headImg" :size="45" />
-      </ABadge>
-      <h4>{{ item.uname }}</h4>
-      <p>{{ item.lastLoginIp }}</p>
-    </ACard>
+    <a-tooltip placement="top" v-for="item in userOnlineStore.userOnline" :key="item.uid">
+      <template #title>
+        <span>{{ item.uname }}{{ item.lastLoginIp }}</span>
+      </template>
+      <ACard :body-style="{padding:'13px'}">
+        <ABadge status="processing" color="green" :dot="true">
+          <AAvatar :src="item.headImg" :size="45"/>
+        </ABadge>
+      </ACard>
+    </a-tooltip>
+
   </div>
 </template>
 
 <style scoped>
 .onliveUser {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
-  grid-template-rows: repeat(auto-fill, minmax(120px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(60px, 1fr));
+  grid-template-rows: repeat(auto-fill, minmax(60px, 1fr));
   gap: 10px;
   height: 400px;
 }
+
 :deep(h4),
 :deep(p) {
   margin: 0;
