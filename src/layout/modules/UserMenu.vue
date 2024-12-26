7c734dc2e981b1f696cd2ad22388b0c7fbc1eb1f
lzyszds <1024327189@qq.com>
2024-12-26 13:19:14 +0800

    ♻️refactor(layout): 重构用户菜单组件并添加新功能

--- src/layout/modules/UserMenu.vue
+++ src/layout/modules/UserMenu.vue
@@ -1,30 +1,49 @@
 <script setup lang="ts">
-import { useUserInfoState } from "@/store/useUserInfoStore";
+import {useUserInfoState} from "@/store/useUserInfoStore";
+import LzyIcon from "@/components/LzyIcon.vue";
 /* 获取用户信息 */
 const userStore = useUserInfoState();
- 
+
+const handleMenuList = [
+  {
+    key: "1",
+    icon: "ph:github-logo",
+    title: "Github",
+    click: () => window.open("https://github.com/lzyszds/blog-admin")
+  },
+  {
+    key: "2",
+    icon: "ph:user-circle-duotone",
+    title: "个人中心",
+    click: userStore.goToUserCenter
+  },
+  {
+    key: "3",
+    icon: "ph:sign-out",
+    title: "退出登陆",
+    click: userStore.logout
+  }
+]
+
 </script>
 
 <template>
   <!-- 个人中心 -->
   <ADropdown :trigger="['click']">
     <AButton type="text">
-      <LzyIcon size="20" name="ph:user-circle-gear" />
+      <LzyIcon size="20" name="ph:user-circle-gear"/>
       <span>
         {{ userStore.userInfo.uname ?? "Jz" }}
       </span>
     </AButton>
     <template #overlay>
       <AMenu>
-        <AMenu-item key="1" @click="userStore.goToUserCenter">
-          <LzyIcon size="18" name="ph:user-circle-duotone" />
-          个人中心
+        <AMenu-item v-for="item in handleMenuList" :key="item.key" @click="item.click">
+          <LzyIcon size="18" :name="item.icon"/>
+          <span style="text-align: center;margin-left: 8px">{{ item.title }}</span>
         </AMenu-item>
-        <AMenu-divider />
-        <AMenu-item key="2" @click="userStore.logout">
-          <LzyIcon size="18" name="ph:sign-out" />
-          退出登陆
-        </AMenu-item>
+        <AMenu-divider/>
+
       </AMenu>
     </template>
   </ADropdown>
