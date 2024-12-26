ee90dc03d6d3d248f680c7f94c87483ad88b6854
lzyszds <1024327189@qq.com>
2024-12-26 17:53:38 +0800

    ✨feat(root): 添加关于项目页面- 新增 About.vue 组件，介绍项目功能和依赖信息 - 更新路由配置，添加关于项目的路由项 - 在 components.d.ts 中添加新使用的 Ant Design 组件类型

--- src/router/config.ts
+++ src/router/config.ts
@@ -1,131 +1,142 @@
-import { RouteRecordRaw } from "vue-router";
+import {RouteRecordRaw} from "vue-router";
 
 export type Meta = {
-  key: number;
-  name: string;
-  uicon: string;
-  componentName?: string;
-  isHide?: boolean;
-  noClose?: boolean;
+    key: number;
+    name: string;
+    uicon: string;
+    componentName?: string;
+    isHide?: boolean;
+    noClose?: boolean;
 };
 
 export type RouteItem = RouteRecordRaw & {
-  meta: Meta;
+    meta: Meta;
 };
 
 const routeItem: RouteItem[] = [
-  {
-    path: "",
-    name: "dashboard",
-    component: () => import("@/views/root/Dashboard.vue"),
-    meta: {
-      key: 1,
-      name: "首页数据",
-      uicon: "solar:pie-chart-2-broken",
-      component: "dashboard",
-      noClose: true,
+    {
+        path: "",
+        name: "dashboard",
+        component: () => import("@/views/root/Dashboard.vue"),
+        meta: {
+            key: 1,
+            name: "首页数据",
+            uicon: "solar:pie-chart-2-broken",
+            component: "dashboard",
+            noClose: true,
+        },
     },
-  },
-  {
-    path: "users",
-    name: "users",
-    component: () => import("@/views/root/Users.vue"),
-    meta: {
-      key: 2,
-      name: "用户管理",
-      uicon: "hugeicons:edit-user-02",
-      component: "users",
+    {
+        path: "users",
+        name: "users",
+        component: () => import("@/views/root/Users.vue"),
+        meta: {
+            key: 2,
+            name: "用户管理",
+            uicon: "hugeicons:edit-user-02",
+            component: "users",
+        },
     },
-  },
-  {
-    path: "posts",
-    name: "posts",
-    component: () => import("@/views/root/Posts.vue"),
-    meta: {
-      key: 3,
-      name: "文章管理",
-      uicon: "hugeicons:property-edit",
-      component: "posts",
+    {
+        path: "posts",
+        name: "posts",
+        component: () => import("@/views/root/Posts.vue"),
+        meta: {
+            key: 3,
+            name: "文章管理",
+            uicon: "hugeicons:property-edit",
+            component: "posts",
+        },
     },
-  },
-  {
-    path: "comments",
-    name: "comments",
-    component: () => import("@/views/root/Comments.vue"),
-    meta: {
-      key: 4,
-      name: "评论管理",
-      uicon: "hugeicons:message-edit-01",
-      component: "comments",
+    {
+        path: "comments",
+        name: "comments",
+        component: () => import("@/views/root/Comments.vue"),
+        meta: {
+            key: 4,
+            name: "评论管理",
+            uicon: "hugeicons:message-edit-01",
+            component: "comments",
+        },
     },
-  },
-  {
-    path: "plantask",
-    name: "plantask",
-    component: () => import("@/views/root/Plantask.vue"),
-    meta: {
-      key: 6,
-      name: "任务队列",
-      uicon: "hugeicons:task-daily-02",
-      component: "plantask",
+    {
+        path: "plantask",
+        name: "plantask",
+        component: () => import("@/views/root/Plantask.vue"),
+        meta: {
+            key: 6,
+            name: "任务队列",
+            uicon: "hugeicons:task-daily-02",
+            component: "plantask",
+        },
     },
-  },
-  {
-    path: "links",
-    name: "links",
-    component: () => import("@/views/root/Links.vue"),
-    meta: {
-      key: 7,
-      name: "友链管理",
-      uicon: "hugeicons:link-01",
-      component: "links",
+    {
+        path: "links",
+        name: "links",
+        component: () => import("@/views/root/Links.vue"),
+        meta: {
+            key: 7,
+            name: "友链管理",
+            uicon: "hugeicons:link-01",
+            component: "links",
+        },
     },
-  },
-  {
-    path: "webset",
-    name: "webset",
-    component: () => import("@/views/root/WebSet.vue"),
-    meta: {
-      key: 8,
-      name: "网站设置",
-      uicon: "hugeicons:computer-settings",
-      component: "webset",
+    {
+        path: "webset",
+        name: "webset",
+        component: () => import("@/views/root/WebSet.vue"),
+        meta: {
+            key: 8,
+            name: "网站设置",
+            uicon: "hugeicons:computer-settings",
+            component: "webset",
+        },
     },
-  },
-  {
-    path: "userCenter",
-    name: "userCenter",
-    component: () => import("@/views/root/UserCenter.vue"),
-    meta: {
-      key: 9,
-      name: "个人中心",
-      uicon: "ph:user-circle-duotone",
-      component: "userCenter",
-      isHide: true,
+    {
+        path: "userCenter",
+        name: "userCenter",
+        component: () => import("@/views/root/UserCenter.vue"),
+        meta: {
+            key: 9,
+            name: "个人中心",
+            uicon: "ph:user-circle-duotone",
+            component: "userCenter",
+            isHide: true,
+        },
     },
-  },
-  {
-    path: "resources",
-    name: "resources",
-    component: () => import("@/views/root/Resources.vue"),
-    meta: {
-      key: 10,
-      name: "资源管理",
-      uicon: "iconoir:media-image-folder",
-      component: "resources",
+    {
+        path: "resources",
+        name: "resources",
+        component: () => import("@/views/root/Resources.vue"),
+        meta: {
+            key: 10,
+            name: "资源管理",
+            uicon: "iconoir:media-image-folder",
+            component: "resources",
+        },
     },
-  },
-  {
-    path: "sysLogs",
-    name: "sysLogs",
-    component: () => import("@/views/root/SysLogs.vue"),
-    meta: {
-      key: 11,
-      name: "系统日志",
-      uicon: "iconoir:calendar",
-      component: "sysLogs",
+    {
+        path: "sysLogs",
+        name: "sysLogs",
+        component: () => import("@/views/root/SysLogs.vue"),
+        meta: {
+            key: 11,
+            name: "系统日志",
+            uicon: "iconoir:calendar",
+            component: "sysLogs",
+        },
     },
-  },
+    {
+        path: "about",
+        name: "about",
+        component: () => import("@/views/root/About.vue"),
+        meta: {
+            key: 12,
+            name: "关于项目",
+            uicon:  "iconoir:rss-feed-tag",
+            component: "about",
+        },
+    },
 ];
 
 // {
---
+++ src/views/root/About.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+import {getTaskLog} from "@/api/plantask.ts";
+
+const {data} = await getTaskLog('e4b4b7bd-cee7-49b7-b1ae-15458e454427', 1)
+
+const result = JSON.parse(data[0].content)
+console.log(result)
+const activeKey = ref('')
+</script>
+
+<template>
+  <main>
+    <a-card title="关于项目" :bordered="false">
+      Jz
+      博客管理系统是一个功能丰富的博客后台管理平台，旨在为用户提供一站式的博客管理解决方案。它提供了全面的数据分析、内容管理、用户管理、评论互动、任务调度、网站设置以及资源管理等功能，帮助用户高效地维护和运营其博客。系统采用模块化设计，界面简洁直观，易于上手。
+    </a-card>
+    <a-card title="生产环境依赖" :bordered="false" style="margin-top: 10px">
+      <a-collapse v-model:activeKey="activeKey" accordion>
+        <a-collapse-panel v-for="(item,index) in result.dependencies" :key="index" :header="index">
+          <p>{{item.description}}<a :href="item.link"  target="_blank">查看文档(由Ai生成可能不准确)</a></p>
+          <template #extra>{{item.version}}</template>
+        </a-collapse-panel>
+      </a-collapse>
+    </a-card>
+    <a-card title="开发环境依赖" :bordered="false" style="margin-top: 10px">
+      <a-collapse v-model:activeKey="activeKey" accordion>
+        <a-collapse-panel v-for="(item,index) in result.devDependencies" :key="index" :header="index">
+          <p>{{item.description}}<a :href="item.link"  target="_blank">查看文档(由Ai生成可能不准确)</a></p>
+          <template #extra>{{item.version}}</template>
+        </a-collapse-panel>
+      </a-collapse>
+    </a-card>
+  </main>
+</template>
+
+<style scoped>
+p {
+  margin: 0;
+}
+</style>
